5426963956ccf7e23be4015a9569eb85d665cfa9
ngohongnguyen01 <ngohongnguyen016774@gmail.com>
2021-10-31 01:53:39 +0700

    update-video

---
+++ app/Constants/S3Constant.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Constants;
+/**
+ * @Author apple
+ * @Date   Oct 04, 2021
+ */
+class S3Constant
+{
+    const LINK_S3 = 'https://duantotnghiep-73.s3.ap-southeast-1.amazonaws.com/';
+}
