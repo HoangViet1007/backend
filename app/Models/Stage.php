777c3cbfbb0cbcc83cd1986302813c54ba3bb783
Hoang Viet <hoangviet10172001@gmail.com>
2021-12-18 00:36:48 +0700

    fix get chi tiet sp

--- app/Models/Stage.php
+++ app/Models/Stage.php
@@ -6,29 +6,30 @@ use App\Constants\StatusConstant;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Stage extends Model
 {
     use HasFactory;
 
-    protected $table = 'stages';
+    protected $table      = 'stages';
     protected $primaryKey = 'id';
-    protected $fillable = ['name', 'short_content', 'content', 'status', 'course_id'];
+    protected $fillable   = ['name', 'short_content', 'content', 'status', 'course_id'];
 
     public function course_planes(): HasMany
     {
-        return $this->hasMany(CoursePlanes::class);
+        return $this->hasMany(CoursePlanes::class,'stage_id','id');
     }
 
     public function course_planes_client(): HasMany
     {
-        return $this->hasMany(CoursePlanes::class)->where('status',StatusConstant::ACTIVE);
+        return $this->hasMany(CoursePlanes::class)
+                    ->where('status', StatusConstant::ACTIVE)
+                    ->select('id','name');
     }
 
     public function course_planes_off(): HasMany
     {
-        return $this->hasMany(CoursePlanes::class)->where('type',0)->where('status',StatusConstant::ACTIVE);
+        return $this->hasMany(CoursePlanes::class)->where('type', 0)->where('status', StatusConstant::ACTIVE);
     }
 
     public function course()
