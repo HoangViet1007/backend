b905af550fec3bcc5b1920a36660fc0bf41f90ac
Chúc Anh Quân <chucquann2011@gmail.com>
2021-12-20 01:12:04 +0700

    update code email verify

--- app/Http/Middleware/EmailVerifyCustom.php
+++ app/Http/Middleware/EmailVerifyCustom.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Middleware;
 
+use App\Exceptions\BadRequestException;
+use App\Models\User;
 use Closure;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Auth;
+use Exception;
 
 class EmailVerifyCustom
 {
@@ -18,16 +22,18 @@ class EmailVerifyCustom
      * @param  string|null  $redirectToRoute
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse|null
      */
-    public function handle($request, Closure $next, $redirectToRoute = null)
+    public function handle($request, Closure $next)
     {
-        if (! $request->user() ||
-            ($request->user() instanceof MustVerifyEmail &&
-                ! $request->user()->hasVerifiedEmail())) {
-            return $request->expectsJson()
-                ? abort(400, 'Email verify.')
-                : "";
+        if(auth()->user()){
+            $user = User::where('id', Auth::id())->first();
+            if($user->email_verified_at === NULL)
+            {
+                throw new BadRequestException(
+                    ['message' => __("email_verify")],
+                    new Exception()
+                );
+            }
         }
-
         return $next($request);
     }
 }
