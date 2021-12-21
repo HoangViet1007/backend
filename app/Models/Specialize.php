6c4b57ebd1ad402c937fa74659901dc21d07eac7
Hoang Viet <hoangviet10172001@gmail.com>
2021-12-22 01:48:56 +0700

    fix bug

--- app/Models/Specialize.php
+++ app/Models/Specialize.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Specialize extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $table    = 'specializes';
     protected $guarded  = [];
